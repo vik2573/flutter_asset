7d08d92fd7aa6d34b1c806b6782bdfb0b7087184
vik2573 <va-demen@yandeex.ru>
2021-06-12 16:00:30 +0400

    App_asset_font Commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: "IndieFlower"),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Adding Asset"),
+          centerTitle: true,
         ),
-        body: null,
+        body: Center(
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/bd.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Image.asset("assets/icons/icon.png"),
+              Positioned(
+                top: 16,
+                left: 115,
+                child: Text(
+                  "My custom font",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    // fontFamily: "IndieFlower",
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
